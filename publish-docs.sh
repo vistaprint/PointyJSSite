32f96c6480362351c76e6dc0b0d184ba715af3b2
Laban Eilers <laban@eilers.com>
2014-02-18 09:31:08 -0500

    Updating publish script

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,10 +1,26 @@
-grunt siteNoVerify
+cd ../pointy
+
+#generate the dist
+grunt build
 
 if [ $? != 0 ]
 then
     exit $?
 fi
 
+cd ../pointysite
+
+# Build the site
+grunt
+
+if [ $? != 0 ]
+then
+    exit $?
+fi
+
+# Go back to pointy, where the gh-pages branch is.
+# We need to put the site there
+cd ../pointy
 git checkout gh-pages
 
 if [ $? != 0 ]
@@ -12,10 +28,11 @@ then
     exit $?
 fi
 
+# Clear the directory of anything that could cause a conflict
 git fetch origin
 git reset --hard origin/gh-pages
 
-cp -R .git/docs-temp/* .
+cp -R ../pointysite/site/_site/* .
 
 git add -A
 git commit -m "Updating documentation"
@@ -25,11 +42,11 @@ then
     exit $?
 fi
 
-git push
+#git push
 
 if [ $? != 0 ]
 then
     exit $?
 fi
 
-git checkout master
+#git checkout master
